93b3aa14c2ccb0e87bd4bcd679c5e5566657e7d2
Daniel Fridkin <danielfridkin@gmail.com>
2018-09-04 21:34:01 -0400

    Added my name to AntLabMain

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -75,6 +75,9 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("P5");
                   toPrint += p5.toString("gburdell5");
+
+                  Person p6 = new Person6("Daniel Fridkin");
+                  toPrint += p6.toString("dfridkin3");
 		  
         System.out.println(toPrint);
         
