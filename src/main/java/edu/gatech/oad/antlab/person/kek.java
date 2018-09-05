1c76030d5893676e3e5d136a2c8facc938de9cf4
nadun <nadun.ranawaka@gatech.edu>
2018-09-05 14:11:12 -0400

    Created tester file. i

---
+++ src/main/java/edu/gatech/oad/antlab/person/kek.java
@@ -0,0 +1,12 @@
+public class kek {
+
+    public static void main (String[] args) {
+
+        Person3 trololol = new Person3 ("Your mom");
+
+        System.out.println(trololol.toString("Barney"));
+
+    }
+
+
+}
